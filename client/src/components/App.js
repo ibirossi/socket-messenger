7b67b80c98aa1e594b773c610d8d0c7e49bdcde9
ibirossi <ibi.homaidan@gmail.com>
2020-12-11 17:57:38 +0100

    Created Conversation Modal and Contacts modañ; created contacts provider and conversation provider contexts; rendered conversation and contacts components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
-import Dashboard from './Dashboard'
+import Dashboard from "./Dashboard";
+import Contacts from "./Contacts";
+import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
-  return (
-  
-    id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
-    
-  )
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
