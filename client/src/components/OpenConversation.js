e2364fe9351c805e02e29bd57a6c62d02d893704
ibirossi <ibi.homaidan@gmail.com>
2021-01-11 12:16:43 +0100

    complete conversations set up, begin send message functionality

---
+++ client/src/components/OpenConversation.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Form, InputGroup, Button } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
+
+function OpenConversation() {
+  const [text, setText] = useState("");
+  const { sendMessage, selectedConversation } = useConversations();
+
+  function handleSend(e) {
+    e.preventDefault();
+
+    sendMessage(
+      selectedConversation.recipients.map((r) => r.id),
+      text
+    )
+    setText('') //clears message box after message is sent
+  }
+
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto"></div>
+      <Form onSubmit={handleSend}>
+        <Form.Group className="m-2">
+          <InputGroup>
+            {/* allows send button to be attached to input*/}
+            <Form.Control
+              as="textarea"
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: "75px", resize: "none" }}
+            />
+            <InputGroup.Append>
+              <Button type="submit">Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
+
+export default OpenConversation;
--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,43 +1,90 @@
-import React, { useContext } from 'react'
-import useLocalStorage from '../hooks/useLocalStorage'
-import { useContacts } from './ContactsProvider'
+import React, { useContext, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { useContacts } from "./ContactsProvider";
 
-const ConversationsContext = React.createContext()
+const ConversationsContext = React.createContext();
 
 export function useConversations() {
-    return useContext(ConversationsContext)
+  return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
-    const [conversations, setConversations] = useLocalStorage('conversations', [])
-    const { contacts } = useContacts()
+export function ConversationsProvider({ id, children }) {
+  const [conversations, setConversations] = useLocalStorage(
+    "conversations",
+    []
+  );
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+  const { contacts } = useContacts();
 
-    function createConversation(recipients) {
-        setConversations(prevConversations => {
-            return [...prevConversations, { recipients, messages: [] }]
-        })
-    }
+  function createConversation(recipients) {
+    setConversations((prevConversations) => {
+      return [...prevConversations, { recipients, messages: [] }];
+    });
+  }
+  //separate function because called from server when message sent and when user sends message.
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations((prevConversations) => {
+      //if no conversation exists with recipient, it will be false => can add new conv.
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConversations.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage],
+          };
+        }
 
-    const formattedConversations = conversations.map(conversation => {
-        const recipients = conversation.recipients.map(recipient => {
-          const contact = contacts.find(contact => {
-              return contact.id === recipient
-          })
-          const name = (contact && contact.name) || recipient
-          return { id: recipient, name}
-        })
+        return conversation;
+      });
 
-        return { ...conversation, recipients }
-    })
+      if (madeChange) {
+        return newConversations;
+      } else {
+        return [...prevConversations, { recipients, messages: [newMessage] }];
+      }
+    });
+  }
 
-    const value = {
-        conversations: formattedConversations, 
-        createConversation
-    }
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id });
+  }
 
-    return (
-       <ConversationsContext.Provider value={{ conversations: formattedConversations, createConversation }}>
-           {children}
-       </ConversationsContext.Provider>
-    )
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+      const name = (contact && contact.name) || recipient;
+      return { id: recipient, name };
+    });
+    const selected = index === selectedConversationIndex; //determine whether conversation is selected or not.
+    return { ...conversation, recipients, selected }; //new conversation
+  });
+
+  const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex], //in order to show selected conversation
+    sendMessage,
+    selectConversationIndex: setSelectedConversationIndex,
+    createConversation,
+  };
+
+  return (
+    <ConversationsContext.Provider value={value}>
+      {children}
+    </ConversationsContext.Provider>
+  );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  a.sort();
+  b.sort();
+
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
