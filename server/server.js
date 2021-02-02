e508da6578650cbfa81f3a72365ad461adbb1b3e
ibirossi <ibi.homaidan@gmail.com>
2021-02-02 11:35:07 +0100

    fixed error:  messages not being received.  Console log => CORS error.  Fixed issue by adding cors config to const io, found in socket.io docs

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 //require socket and call which port to use (5000).
-const io = require("socket.io")(5000);
+//const io = require("socket.io")(5000);
+const io = require("socket.io")(5000, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
 io.on("connection", (socket) => {
   // when connect will pass id of user.
   //Socket creates a new id for each connection.
   //This will allow to make a static id that remains when page refreshes.
+  //id is sent from useEffect in SocketProvider
   const id = socket.handshake.query.id;
   socket.join(id);
 
   socket.on("send-message", ({ recipients, text }) => {
-    //loop through recipients.
+    //1. 'send-message' sent from client ConvProvider
+    //2. loop through recipients.
     //Need to change recipient depending on who is sending and receiving.
     recipients.forEach((recipient) => {
       //removes current recipient(i.e. the sender) from list of recipients
       const newRecipients = recipients.filter((r) => r !== recipient);
       //id = sender of message
       newRecipients.push(id);
+      //3.'receive-message' sent to each recipient, need to pick up on client side. 
       socket.broadcast.to(recipient).emit("receive-message", {
         recipients: newRecipients,
         sender: id,
